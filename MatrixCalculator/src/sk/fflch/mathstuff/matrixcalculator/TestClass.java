c890ddc0ad23013f13f64782efa71c2ff2ceef94
LukasCh <lukchmelar@gmail.com>
2013-10-12 11:20:18 +0200

    First commit.

---
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
@@ -0,0 +1,10 @@
+package sk.fflch.mathstuff.matrixcalculator;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Math! No go and die you piece of shit!");
+	}
+
+}
