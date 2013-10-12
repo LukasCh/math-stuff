8a5ff495df281fc7611448bd8123d4351c14560e
Frantisekf <frantisekfarkas93@gmail.com>
2013-10-12 12:52:59 +0200

    scitanie- done

--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
@@ -1,10 +1,20 @@
 package sk.fflch.mathstuff.matrixcalculator;
 
+import java.util.Arrays;
+
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Math! Now go and die you piece of shit!");
+		int[][] matrixA = { { 1, 2 }, { 3, 4 }, { 2, 1 } };
+		int[][] matrixB = { { 2, 4 }, { -5, 6 }, { -2, 0 } };
+
+		int[][] matrixC = UltimateMatrixCalculator
+				.addMatrices(matrixA, matrixB);
+		System.out.println(Arrays.deepToString(matrixC).replaceAll("],",
+				"],\r\n"));
+
 	}
 
 }
--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
@@ -1,10 +1,24 @@
 package sk.fflch.mathstuff.matrixcalculator;
 
 public class UltimateMatrixCalculator {
-	
+
 	public static int[][] addMatrices(int[][] matrixA, int[][] matrixB) {
 		int[][] result = null;
-		
-		return result;		
+		System.out.println(matrixA.length + " " + matrixA[0].length);
+		System.out.println(matrixB.length + " " + matrixB[0].length);
+
+		if (matrixA.length == matrixB.length
+				&& matrixA[0].length == matrixB[0].length) {
+			result = new int[matrixA.length][matrixA[0].length];
+			for (int x = 0; x < matrixA.length; x++) {
+				for (int y = 0; y < matrixA[x].length; y++) {
+					result[x][y] = matrixA[x][y] + matrixB[x][y];
+					// System.out.println(x+" "+y);
+				}
+			}
+		} else {
+			System.out.println("matice musia mat rovnaku velkost ty retard..");
+		}
+		return result;
 	}
 }
