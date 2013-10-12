9adec82981ac219de5b949ab07616e7fc195182e
Frantisekf <frantisekfarkas93@gmail.com>
2013-10-12 13:04:54 +0200

    Matrix substraction implemented

--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
@@ -9,12 +9,16 @@ public class TestClass {
 		System.out.println("Hello Math! Now go and die you piece of shit!");
 		int[][] matrixA = { { 1, 2 }, { 3, 4 }, { 2, 1 } };
 		int[][] matrixB = { { 2, 4 }, { -5, 6 }, { -2, 0 } };
-
+        System.out.println("adding");
 		int[][] matrixC = UltimateMatrixCalculator
 				.addMatrices(matrixA, matrixB);
 		System.out.println(Arrays.deepToString(matrixC).replaceAll("],",
 				"],\r\n"));
-
+		System.out.println("substracting");
+		int[][] matrixD = UltimateMatrixCalculator
+				.subMatrices(matrixA, matrixB);
+		System.out.println(Arrays.deepToString(matrixD).replaceAll("],",
+				"],\r\n"));
 	}
 
 }
--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
@@ -21,4 +21,24 @@ public class UltimateMatrixCalculator {
 		}
 		return result;
 	}
+
+	public static int[][] subMatrices(int[][] matrixA, int[][] matrixB) {
+		int[][] result = null;
+		System.out.println(matrixA.length + " " + matrixA[0].length);
+		System.out.println(matrixB.length + " " + matrixB[0].length);
+
+		if (matrixA.length == matrixB.length
+				&& matrixA[0].length == matrixB[0].length) {
+			result = new int[matrixA.length][matrixA[0].length];
+			for (int x = 0; x < matrixA.length; x++) {
+				for (int y = 0; y < matrixA[x].length; y++) {
+					result[x][y] = matrixA[x][y] - matrixB[x][y];
+					// System.out.println(x+" "+y);
+				}
+			}
+		} else {
+			System.out.println("matice musia mat rovnaku velkost ty retard..");
+		}
+		return result;
+	}
 }
