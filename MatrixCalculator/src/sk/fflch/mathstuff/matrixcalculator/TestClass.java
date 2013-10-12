6c95865e392522cf1ff587198fb7985a630b4608
Frantisekf <frantisekfarkas93@gmail.com>
2013-10-12 16:27:15 +0200

    multiplication by number and multiplication by another matrice - finished

--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/TestClass.java
@@ -6,19 +6,22 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("OK*sigh* I'll do the math for you");
+		System.out.println("OK *sigh* I'll do the math for you");
 		int[][] matrixA = { { 1, 2 }, { 3, 4 }, { 2, 1 } };
 		int[][] matrixB = { { 2, 4 }, { -5, 6 }, { -2, 0 } };
-        System.out.println("adding");
-		int[][] matrixC = UltimateMatrixCalculator
-				.addMatrices(matrixA, matrixB);
-		System.out.println(Arrays.deepToString(matrixC).replaceAll("],",
-				"],\r\n"));
+		int[][] matrixB2 = { { 3, 6, 4 }, { 7, 2, 1 } };
+		System.out.println("adding");
+		int[][] matrixC = UltimateMatrixCalculator.addMatrices(matrixA, matrixB);
+		System.out.println(Arrays.deepToString(matrixC).replaceAll("],", "],\r\n"));
 		System.out.println("substracting");
-		int[][] matrixD = UltimateMatrixCalculator
-				.subMatrices(matrixA, matrixB);
-		System.out.println(Arrays.deepToString(matrixD).replaceAll("],",
-				"],\r\n"));
-	}
+		int[][] matrixD = UltimateMatrixCalculator.subMatrices(matrixA, matrixB);
+		System.out.println(Arrays.deepToString(matrixD).replaceAll("],", "],\r\n"));
+		System.out.println("multiply by number");
+		int[][] matrixM = UltimateMatrixCalculator.mulMatrix(matrixA, 8);
+		System.out.println(Arrays.deepToString(matrixM).replaceAll("],", "],\r\n"));
+		System.out.println("multiply matrices");
+		int[][] matrixL = UltimateMatrixCalculator.mulMatrices(matrixA, matrixB2);
+		System.out.println(Arrays.deepToString(matrixL).replaceAll("],", "],\r\n"));
 
+	}
 }
--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
@@ -4,11 +4,8 @@ public class UltimateMatrixCalculator {
 
 	public static int[][] addMatrices(int[][] matrixA, int[][] matrixB) {
 		int[][] result = null;
-		System.out.println(matrixA.length + " " + matrixA[0].length);
-		System.out.println(matrixB.length + " " + matrixB[0].length);
 
-		if (matrixA.length == matrixB.length
-				&& matrixA[0].length == matrixB[0].length) {
+		if (matrixA.length == matrixB.length && matrixA[0].length == matrixB[0].length) {
 			result = new int[matrixA.length][matrixA[0].length];
 			for (int x = 0; x < matrixA.length; x++) {
 				for (int y = 0; y < matrixA[x].length; y++) {
@@ -24,11 +21,8 @@ public class UltimateMatrixCalculator {
 
 	public static int[][] subMatrices(int[][] matrixA, int[][] matrixB) {
 		int[][] result = null;
-		System.out.println(matrixA.length + " " + matrixA[0].length);
-		System.out.println(matrixB.length + " " + matrixB[0].length);
 
-		if (matrixA.length == matrixB.length
-				&& matrixA[0].length == matrixB[0].length) {
+		if (matrixA.length == matrixB.length && matrixA[0].length == matrixB[0].length) {
 			result = new int[matrixA.length][matrixA[0].length];
 			for (int x = 0; x < matrixA.length; x++) {
 				for (int y = 0; y < matrixA[x].length; y++) {
@@ -40,5 +34,39 @@ public class UltimateMatrixCalculator {
 			System.out.println("matice musia mat rovnaku velkost ty retard..");
 		}
 		return result;
+
+	}
+
+	public static int[][] mulMatrix(int[][] matrix, int i) {
+		int[][] result = new int[matrix.length][matrix[0].length];
+		for (int x = 0; x < result.length; x++) {
+			for (int y = 0; y < result[x].length; y++) {
+				result[x][y] = matrix[x][y] * i;
+			}
+		}
+
+		return result;
+
+	}
+
+	public static int[][] mulMatrices(int[][] matrixA, int[][] matrixB) {
+		int[][] result = new int[matrixA.length][matrixB[0].length];
+		if (matrixA.length == matrixB[0].length && matrixA[0].length == matrixB.length) {
+			for (int x = 0; x < result.length; x++) {
+				for (int y = 0; y < result[x].length; y++) {
+					int dotProduct = 0;
+					for (int z = 0; z < matrixA[x].length; z++) {
+						dotProduct = dotProduct + matrixA[x][z] * matrixB[z][y];
+					}
+
+					result[x][y] = dotProduct;
+				}
+			}
+
+		} else {
+			System.out.println("Sa uvedom nemoze to tak byt... skoc z okna prosim ta");
+		}
+		return result;
+
 	}
 }
