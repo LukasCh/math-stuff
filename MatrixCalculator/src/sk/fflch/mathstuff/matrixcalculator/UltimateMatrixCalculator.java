263ceaa6ee54c5a731a56c1568e8abc6c07d17a7
LukasCh <lukchmelar@gmail.com>
2013-10-12 11:32:32 +0200

    Ultimate Matrix Calculator added

---
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/UltimateMatrixCalculator.java
@@ -0,0 +1,10 @@
+package sk.fflch.mathstuff.matrixcalculator;
+
+public class UltimateMatrixCalculator {
+	
+	public static int[][] addMatrices(int[][] matrixA, int[][] matrixB) {
+		int[][] result = null;
+		
+		return result;		
+	}
+}
