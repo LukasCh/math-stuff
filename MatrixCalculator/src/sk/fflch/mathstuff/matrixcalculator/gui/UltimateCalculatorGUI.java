2ef57a0fd10598f677e76d4a006d5659c7352d16
Frantisekf <frantisekfarkas93@gmail.com>
2013-10-14 21:36:33 +0200

    mulMatrix by number added to the GUI

--- MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/gui/UltimateCalculatorGUI.java
+++ MatrixCalculator/src/sk/fflch/mathstuff/matrixcalculator/gui/UltimateCalculatorGUI.java
@@ -21,28 +21,28 @@ public class UltimateCalculatorGUI extends JFrame {
 	private JTextArea firstTextarea;
 	private JTextArea secondTextarea;
 	private JTextArea resultTextarea;
-	
+
 	private static String firstTextareaText = "1 2 3\n4 5 6\n7 8 9";
 	private static String secondTextareaText = "1 2 3\n4 5 6\n7 8 9";
-	
+
 	public UltimateCalculatorGUI() {
 		initWindow();
 	}
-	
+
 	private void initWindow() {
 		setTitle("Ultimate Calculator Window");
 		setSize(500, 800);
-		add(getContent());	
+		add(getContent());
 		setVisible(true);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
+
 	public static void main(String[] args) {
 		UltimateCalculatorGUI gui = new UltimateCalculatorGUI();
 	}
-	
+
 	private JPanel getContent() {
 		JPanel panel = new JPanel();
 		panel.add(new JLabel("Textarea #1"));
@@ -55,82 +55,100 @@ public class UltimateCalculatorGUI extends JFrame {
 		secondTextarea.setPreferredSize(new Dimension(450, 200));
 		secondTextarea.setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		panel.add(secondTextarea);
-		
+
 		JButton addMatricesButton = new JButton("Add Matrices");
-		addMatricesButton.addActionListener(new ActionListener() {			
+		addMatricesButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				int[][] matrixA = textToMatrix(firstTextarea.getText());
 				int[][] matrixB = textToMatrix(secondTextarea.getText());
 				int[][] result = UltimateMatrixCalculator.addMatrices(matrixA, matrixB);
 				resultTextarea.setText(getMatrixDisplayString(result));
-				
+
 			}
 		});
 		panel.add(addMatricesButton);
-		
+
 		JButton subMatricesButton = new JButton("Sub Matrices");
-		subMatricesButton.addActionListener(new ActionListener() {			
+		subMatricesButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("Sub Matrices Pressed");
-				
+				int[][] matrixA = textToMatrix(firstTextarea.getText());
+				int[][] matrixB = textToMatrix(secondTextarea.getText());
+				int[][] result = UltimateMatrixCalculator.subMatrices(matrixA, matrixB);
+				resultTextarea.setText(getMatrixDisplayString(result));
 			}
 		});
 		panel.add(subMatricesButton);
-		
+
 		JButton mulMatricesButton = new JButton("Mul Matrices");
-		mulMatricesButton.addActionListener(new ActionListener() {			
+		mulMatricesButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("Mul Matrices Pressed");
-				
+				int[][] matrixA = textToMatrix(firstTextarea.getText());
+				int[][] result = null;
+				if (isNumeric(secondTextarea.getText())) {
+					result = UltimateMatrixCalculator.mulMatrix(matrixA, Integer.parseInt(secondTextarea.getText()));
+				} else {
+					int[][] matrixB = textToMatrix(secondTextarea.getText());
+					result = UltimateMatrixCalculator.mulMatrices(matrixA, matrixB);
+				}
+				resultTextarea.setText(getMatrixDisplayString(result));
+
 			}
 		});
 		panel.add(mulMatricesButton);
 		panel.add(new JLabel("                                                                            "));
 		panel.add(new JLabel("                                       Result                                       "));
-		
+
 		resultTextarea = new JTextArea();
 		resultTextarea.setPreferredSize(new Dimension(450, 200));
 		resultTextarea.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-		resultTextarea.setEditable(false);		
+		resultTextarea.setEditable(false);
 		panel.add(resultTextarea);
-		
+
 		return panel;
 	}
-	
-	public static int[][] textToMatrix(String text){
+
+	public static int[][] textToMatrix(String text) {
 		int[][] matrix = null;
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		
+
 		Scanner textScanner = new Scanner(text);
 		int rowNumber = 0;
-		
-		while(textScanner.hasNextLine()) {		
+
+		while (textScanner.hasNextLine()) {
 			Scanner rowScanner = new Scanner(textScanner.nextLine()).useDelimiter(" ");
 			result.add(new ArrayList<Integer>());
-			while(rowScanner.hasNextInt()) {
+			while (rowScanner.hasNextInt()) {
 				result.get(rowNumber).add(rowScanner.nextInt());
-			}			
+			}
 			rowScanner.close();
 			rowNumber++;
 		}
 		textScanner.close();
-		
+
 		matrix = new int[result.size()][result.get(0).size()];
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
 				matrix[i][j] = result.get(i).get(j);
-			}			
+			}
 		}
 		result.toArray();
-		
+
 		return matrix;
 	}
-	
+
 	public static String getMatrixDisplayString(int[][] matrix) {
-		return Arrays.deepToString(matrix).replaceAll("],", "],\r\n").replace(" [", "").replace("]", "").replace("[", "").replace(",", "").replace(" ","\t");
+		return Arrays.deepToString(matrix).replaceAll("],", "],\r\n").replace(" [", "").replace("]", "").replace("[", "").replace(",", "").replace(" ", "\t");
+	}
+
+	public static boolean isNumeric(String str) {
+		try {
+			int d = Integer.parseInt(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
 	}
 }
-
